95672a53be312f2690d7d50b214827255f14b5ca
denervalspelizario <denervalspelizario@gmail.com>
2023-03-08 21:21:05 -0300

    update appChelseaFc estilizacao screen login

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,65 +1,175 @@
-import React from 'react'; // 1 importando o useRef
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native'; 
+import React from 'react'; 
+import { StyleSheet, 
+    Text,
+    View,
+    TouchableOpacity,
+    SafeAreaView,
+    Image,
+    TextInput } from 'react-native'; 
 import { useNavigation } from '@react-navigation/native'; // 3 importando elemento para navegação
+import LogoChelsea from '../../../assets/Chelsea_FC.svg.png'
+import AppLoading from 'expo-app-loading';
+import {
+  useFonts,
+  Roboto_100Thin,
+  Roboto_100Thin_Italic,
+  Roboto_300Light,
+  Roboto_300Light_Italic,
+  Roboto_400Regular,
+  Roboto_400Regular_Italic,
+  Roboto_500Medium,
+  Roboto_500Medium_Italic,
+  Roboto_700Bold,
+  Roboto_700Bold_Italic,
+  Roboto_900Black,
+  Roboto_900Black_Italic,
+} from '@expo-google-fonts/roboto';
+import { FontAwesome } from '@expo/vector-icons';
+
+
 
 export default function Login(){
 
  const navigation = useNavigation();  // 3 adicionando elemento a funcao navigation
+
+ let [fontsLoaded] = useFonts({
+  Roboto_100Thin,
+  Roboto_100Thin_Italic,
+  Roboto_300Light,
+  Roboto_300Light_Italic,
+  Roboto_400Regular,
+  Roboto_400Regular_Italic,
+  Roboto_500Medium,
+  Roboto_500Medium_Italic,
+  Roboto_700Bold,
+  Roboto_700Bold_Italic,
+  Roboto_900Black,
+  Roboto_900Black_Italic,
+});
+
+let fontSize = 24;
+let paddingVertical = 6;
+
+if (!fontsLoaded) {
+  return <AppLoading />;
+} else {
                                                                           
   return(
-    <View style={styles.container}>
+    <SafeAreaView style={styles.container}>
 
-        <Text style={styles.textContainer}>
-          Pagina de Login
+      <View style={styles.logoContainer}>
+        <Image
+          source={LogoChelsea}
+          style={styles.logoStyle}
+        />
+        <Text style={styles.textLogo}>
+          CHELSEA FC APP
         </Text>
+      </View>
+
+      <View style={styles.inputContainer}>
+        <FontAwesome style={styles.icon} name="user" size={20} color="#FFF" />
+        <TextInput
+          style={styles.input}  
+          placeholder='Email' 
+          underlineColorAndroid="transparent"   
+          keyboardType='email-address'  
+          maxLength={120}  
         
-        <TouchableOpacity 
+        />
+      </View>
+      
+      <View style={styles.inputContainer}>
+        <FontAwesome style={styles.icon} name="lock" size={20} color="#FFF" />
+        <TextInput
+          style={styles.input}  
+          placeholder="Password"
+          secureTextEntry={true} 
+          autoCorrect={false} 
+          textContentType={'password'}
+          
+        />
+      </View>
+
+      <TouchableOpacity 
           onPress={()=> navigation.navigate('HomeStack')} /* ao clicar navega a pagina Sobre (ver linha 7 desta pagina e 44 de app.js)*/   
-          style={styles.btnHome}   
+          style={styles.btnLogin}   
         >
-        <Text style={styles.textBtn}>Ir para Home</Text>
+          <Text style={styles.textBtn}>Login</Text>
         </TouchableOpacity>
 
-        <Text style={styles.text}>Cadastro</Text>
+        
+        
         <TouchableOpacity 
           onPress={()=> navigation.navigate('Sign')} /* ao clicar navega a pagina Sobre (ver linha 7 desta pagina e 44 de app.js)*/   
           style={styles.btnCadastro}    
         >
-        <Text style={styles.textBtn}>Ir para Cadastro</Text>
+        <Text style={styles.textCadastro}>Cadastrar nova conta?</Text>
         </TouchableOpacity>
         
-    </View>
+    </SafeAreaView>
 
-  )
+   )
+
+  }
 
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#034694',
+  },
+  logoContainer: {
+    marginTop: '30%',
+    marginBottom: '15%',
     alignItems: 'center',
   },
-  textContainer: {
+  logoStyle: {
+    width: 100,
+    height: 100,
+  },
+  textLogo: {
+    marginTop: 15,
+    color: '#FFF',
+    textAlign: 'center',
+    fontSize: 20,
+    fontFamily: 'Roboto_900Black_Italic',
+  },
+  inputContainer: {
+    display: 'flex',
+    flexDirection: 'row',
+    borderBottomWidth: 1,
+    borderBottomColor: '#FFF',
+    width: 200,
     marginBottom: 20,
   },
-  btnHome: {
-    backgroundColor: '#4169E1',
+  input: {
+    marginLeft: 10,
+  },
+  btnLogin: {
+    backgroundColor: '#FFF',
     paddingHorizontal: 10,
     paddingVertical: 10,
-    borderRadius: 10,
+    borderRadius: 20,
+    width: 100,
   },
   textBtn: {
-    color: '#FFF',
+    color: '#034694',
+    textAlign: 'center',
+    fontSize: 15,
+    fontFamily: 'Roboto_700Bold',
   },
   btnCadastro: {
-    backgroundColor: '#4169',
-    paddingHorizontal: 10,
-    paddingVertical: 10,
-    borderRadius: 10,
+   marginTop: '25%'
   },
-  text: {
-    marginVertical: 15,
+  textCadastro: {
+    color: '#FFF',
+    fontFamily: 'Roboto_700Bold',
+
   }
+
+  
   
 });
