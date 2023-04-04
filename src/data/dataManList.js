b911f1295067bbbad05d9b68cd1b0a8cfff892b0
denervalspelizario <denervalspelizario@gmail.com>
2023-04-03 23:05:28 -0300

    update appChelseaFC adicao completa de array de dataManList

--- src/data/dataManList.js
+++ src/data/dataManList.js
@@ -4,13 +4,42 @@ import ImageMan2 from '../../assets/Team/manList/goleiros/gabrielSlonina.png';
 import ImageMan3 from '../../assets/Team/manList/goleiros/kepaarrizabalaga.png';
 import ImageMan4 from '../../assets/Team/manList/goleiros/marcusBettinelli.png';
 
+import ImageMan5 from '../../assets/Team/manList/zagueiros/benoitBadiashile.png';
+import ImageMan6 from '../../assets/Team/manList/zagueiros/kalidouKoulibaly.png';
+import ImageMan7 from '../../assets/Team/manList/zagueiros/thiagoSilva.png'
+import ImageMan8 from '../../assets/Team/manList/zagueiros/trevohChalobah.png';
+import ImageMan9 from '../../assets/Team/manList/zagueiros/wesleyFofana.png';
 
+import ImageMan10 from '../../assets/Team/manList/laterais/benChilwell.png';
+import ImageMan11 from '../../assets/Team/manList/laterais/cesarAzpilicueta.png';
+import ImageMan12 from '../../assets/Team/manList/laterais/marcCucurella.png';
+import ImageMan13 from '../../assets/Team/manList/laterais/reeceJames.png';
 
+import ImageMan14 from '../../assets/Team/manList/Meias/carneyChukwuemeka.png';
+import ImageMan15 from '../../assets/Team/manList/Meias/conorGallagher.png';
+import ImageMan16 from '../../assets/Team/manList/Meias/denisZakaria.png';
+import ImageMan17 from '../../assets/Team/manList/Meias/enzoFernandez.png';
+import ImageMan18 from '../../assets/Team/manList/Meias/masonMount.png';
+import ImageMan19 from '../../assets/Team/manList/Meias/mateoKovacic.png';
+import ImageMan20 from '../../assets/Team/manList/Meias/ngoloKante.png';
+import ImageMan21 from '../../assets/Team/manList/Meias/rubenLoftusCheek.png';
+
+import ImageMan22 from '../../assets/Team/manList/Ataque/armandoBroja.png';
+import ImageMan23 from '../../assets/Team/manList/Ataque/cristianPulisic.png';
+import ImageMan24 from '../../assets/Team/manList/Ataque/davidFofana.png';
+import ImageMan25 from '../../assets/Team/manList/Ataque/hakimZyech.png';
+import ImageMan26 from '../../assets/Team/manList/Ataque/joaoFelix.png';
+import ImageMan27 from '../../assets/Team/manList/Ataque/kaiHavertz.png';
+import ImageMan28 from '../../assets/Team/manList/Ataque/mykhailoMudrik.png';
+import ImageMan29 from '../../assets/Team/manList/Ataque/noniMadueke.png';
+import ImageMan30 from '../../assets/Team/manList/Ataque/pierreAubameyang.png';
+import ImageMan31 from '../../assets/Team/manList/Ataque/raheemSterling.png';
 
 
 
 export const dataManList = [
   {
+    // Goleiros
     key: '1', newImage: ImageMan3 , name: 'Kepa', subName: 'Arrizabalaga',
     number: '1', position: 'Goleiro'
   },
@@ -26,4 +55,121 @@ export const dataManList = [
     key: '4', newImage: ImageMan2 , name: 'Slonina', subName: 'Gabriel',
     number: '36', position: 'Goleiro'
   },  
+
+  //Zagueiros
+  {
+    key: '5', newImage: ImageMan5 , name: 'Badiashile', subName: 'benoit',
+    number: '4', position: 'Zagueiro'
+  },
+  {
+    key: '6', newImage: ImageMan6 , name: 'Koulibaly', subName: 'kalidou',
+    number: '26', position: 'Zagueiro'
+  },
+  {
+    key: '7', newImage: ImageMan7 , name: 'Silva', subName: 'Tiago',
+    number: '6', position: 'Zagueiro'
+  },
+  {
+    key: '8', newImage: ImageMan8 , name: 'Chalobah', subName: 'trevoh',
+    number: '14', position: 'Zagueiro'
+  },
+  {
+    key: '9', newImage: ImageMan9 , name: 'Fofana', subName: 'Wesley',
+    number: '33', position: 'Zagueiro'
+  },   
+
+  // laterais 
+
+  {
+    key: '10', newImage: ImageMan10 , name: 'Chilwell', subName: 'Ben',
+    number: '21', position: 'Lateral'
+  },
+  {
+    key: '11', newImage: ImageMan11 , name: 'Azpilicueta', subName: 'Cezar',
+    number: '28', position: 'Lateral'
+  },
+  {
+    key: '12', newImage: ImageMan12 , name: 'Cucurella', subName: 'Marc',
+    number: '32', position: 'Lateral'
+  },
+  {
+    key: '13', newImage: ImageMan13 , name: 'James', subName: 'Reece',
+    number: '24', position: 'Lateral'
+  }, 
+  
+  // Meias
+  {
+    key: '14', newImage: ImageMan14 , name: 'Chukwuemeka', subName: 'carney',
+    number: '30', position: 'Meia'
+  },
+  {
+    key: '15', newImage: ImageMan15 , name: 'Gallagher', subName: 'Connor',
+    number: '23', position: 'Meia'
+  },
+  {
+    key: '16', newImage: ImageMan16 , name: 'Zakaria', subName: 'Denis',
+    number: '20', position: 'Meia'
+  },
+  {
+    key: '17', newImage: ImageMan17 , name: 'Fernandez', subName: 'Enzo',
+    number: '5', position: 'Meia'
+  }, 
+  {
+    key: '18', newImage: ImageMan18 , name: 'Mount', subName: 'Mason',
+    number: '19', position: 'Meia'
+  },
+  {
+    key: '19', newImage: ImageMan19 , name: 'Kovacic', subName: 'Mateo',
+    number: '8', position: 'Meia'
+  },
+  {
+    key: '20', newImage: ImageMan20 , name: 'Kante', subName: 'Ngolo',
+    number: '7', position: 'Meia'
+  },
+  {
+    key: '21', newImage: ImageMan21 , name: 'LoftusChee', subName: 'Ruben',
+    number: '12', position: 'Meia'
+  }, 
+  
+  // Ataque
+  {
+    key: '22', newImage: ImageMan22 , name: 'Broja', subName: 'Armando',
+    number: '18', position: 'Ataque'
+  },
+  {
+    key: '23', newImage: ImageMan23 , name: 'Pulisic', subName: 'Cristian',
+    number: '10', position: 'Ataque'
+  },
+  {
+    key: '24', newImage: ImageMan24 , name: 'Fofana', subName: 'David',
+    number: '27', position: 'Ataque'
+  },
+  {
+    key: '25', newImage: ImageMan25 , name: 'Zyech', subName: 'Hakim',
+    number: '22', position: 'Ataque'
+  }, 
+  {
+    key: '26', newImage: ImageMan26 , name: 'Felix', subName: 'João',
+    number: '11', position: 'Ataque'
+  },
+  {
+    key: '27', newImage: ImageMan27 , name: 'Havertz', subName: 'Kai',
+    number: '29', position: 'Ataque'
+  },
+  {
+    key: '28', newImage: ImageMan28 , name: 'Mudrik', subName: 'Mykhailo',
+    number: '15', position: 'Meia'
+  },
+  {
+    key: '29', newImage: ImageMan29 , name: 'Madueke', subName: 'Noni',
+    number: '31', position: 'Ataque'
+  },
+  {
+    key: '30', newImage: ImageMan30 , name: 'Aubameyang', subName: 'Pierre',
+    number: '9', position: 'Ataque'
+  },
+  {
+    key: '31', newImage: ImageMan31 , name: 'Sterling', subName: 'Raheem',
+    number: '17', position: 'Ataque'
+  },
 ]
