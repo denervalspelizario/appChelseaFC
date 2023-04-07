9b84c6d36c766f7f01ae5e661f793943fcd5d907
denervalspelizario <denervalspelizario@gmail.com>
2023-04-06 22:09:15 -0300

    update appChelseaFC criacao de dataWomanList ja com tdoas as infos e imagens do time feminino

---
+++ src/data/dataWomanList.js
@@ -0,0 +1,134 @@
+
+import ImageWoman1 from '../../assets/Team/womanList/goleiros/annBerger.png';
+import ImageWoman2 from '../../assets/Team/womanList/goleiros/emilyOrman.png';
+import ImageWoman3 from '../../assets/Team/womanList/goleiros/zeciraMusovic.png';
+
+
+import ImageWoman4 from '../../assets/Team/womanList/zagueiros/alsuAbdulina.png';
+import ImageWoman5 from '../../assets/Team/womanList/zagueiros/evePerisset.png';
+import ImageWoman6 from '../../assets/Team/womanList/zagueiros/jessCarter.png';
+import ImageWoman7 from '../../assets/Team/womanList/zagueiros/kadeishaBuchanan.png'
+import ImageWoman8 from '../../assets/Team/womanList/zagueiros/magdalenaEriksson.png';
+import ImageWoman9 from '../../assets/Team/womanList/zagueiros/marenMjelde.png';
+import ImageWoman10 from '../../assets/Team/womanList/zagueiros/millieBringht.png';
+import ImageWoman11 from '../../assets/Team/womanList/zagueiros/niamhCharles.png';
+
+import ImageWoman12 from '../../assets/Team/womanList/Meias/erinCuthbert.png';
+import ImageWoman13 from '../../assets/Team/womanList/Meias/guroReiten.png';
+import ImageWoman14 from '../../assets/Team/womanList/Meias/jelenaCankovic.png';
+import ImageWoman15 from '../../assets/Team/womanList/Meias/jessieFleming.png';
+import ImageWoman16 from '../../assets/Team/womanList/Meias/katerinaSvitkova.png';
+import ImageWoman17 from '../../assets/Team/womanList/Meias/melanieLeupolz.png';
+import ImageWoman18 from '../../assets/Team/womanList/Meias/pernilleHarder.png';
+import ImageWoman19 from '../../assets/Team/womanList/Meias/sophieIngle.png';
+
+import ImageWoman20 from '../../assets/Team/womanList/Ataque/franKirby.png';
+import ImageWoman21 from '../../assets/Team/womanList/Ataque/johannaKaneryd.png';
+import ImageWoman22 from '../../assets/Team/womanList/Ataque/laurenJames.png';
+import ImageWoman23 from '../../assets/Team/womanList/Ataque/samKerr.png';
+
+
+
+export const dataManList = [
+  {
+    // Goleiros
+    key: '1', newImage: ImageWoman1 , name: 'Berger', subName: 'Anne',
+    number: '30', position: 'Goalkeeper'
+  },
+  {
+    key: '2', newImage: ImageWoman2 , name: 'Orman', subName: 'Emily',
+    number: '32', position: 'Goalkeeper'
+  },
+  {
+    key: '3', newImage: ImageWoman3 , name: 'Musovic', subName: 'Zecira',
+    number: '1', position: 'Goalkeeper'
+  }, 
+
+  //Zagueiros
+  {
+    key: '4', newImage: ImageWoman4 , name: 'Abdulina', subName: 'Alsu',
+    number: '27', position: 'Defender'
+  }, 
+  {
+    key: '5', newImage: ImageWoman5 , name: 'Perisset', subName: 'Eve',
+    number: '15', position: 'Defender'
+  },
+  {
+    key: '6', newImage: ImageWoman6 , name: 'Carter', subName: 'Jess',
+    number: '7', position: 'Defender'
+  },
+  {
+    key: '7', newImage: ImageWoman7 , name: 'Buchanan', subName: 'Kadeisha',
+    number: '26', position: 'Defender'
+  },
+  {
+    key: '8', newImage: ImageWoman8 , name: 'Eriksson', subName: 'Magdalena',
+    number: '16', position: 'Defender'
+  },
+  {
+    key: '9', newImage: ImageWoman9 , name: 'Mjeld', subName: 'Maren',
+    number: '18', position: 'Defender'
+  }, 
+  {
+    key: '10', newImage: ImageWoman10 , name: 'Bringht', subName: 'Millie',
+    number: '04', position: 'Defender'
+  },
+  {
+    key: '11', newImage: ImageWoman11 , name: 'Charles', subName: 'Niamh',
+    number: '21', position: 'Defender'
+  },  
+
+// Meias
+  {
+    key: '12', newImage: ImageWoman12 , name: 'Cuthbert', subName: 'Erin',
+    number: '22', position: 'Midfielder'
+  },
+  {
+    key: '13', newImage: ImageWoman13 , name: 'Reiten', subName: 'Guro',
+    number: '11', position: 'Midfielder'
+  }, 
+  {
+    key: '14', newImage: ImageWoman14 , name: 'Cankovic', subName: 'Jelena',
+    number: '28', position: 'Midfielder'
+  },
+  {
+    key: '15', newImage: ImageWoman15 , name: 'Fleming', subName: 'Jessie',
+    number: '17', position: 'Midfielder'
+  },
+  {
+    key: '16', newImage: ImageWoman16 , name: 'Svitkova', subName: 'Katerina',
+    number: '13', position: 'Midfielder'
+  },
+  {
+    key: '17', newImage: ImageWoman17 , name: 'Leupolz', subName: 'Melanie',
+    number: '08', position: 'Midfielder'
+  }, 
+  {
+    key: '18', newImage: ImageWoman18 , name: 'Harder', subName: 'Pernille',
+    number: '23', position: 'Midfielder'
+  },
+  {
+    key: '19', newImage: ImageWoman19 , name: 'Ingle', subName: 'Sophie',
+    number: '05', position: 'Midfielder'
+  },
+   
+  
+  // Ataque
+  {
+    key: '20', newImage: ImageWoman20 , name: 'Kirby', subName: 'Fran',
+    number: '14', position: 'Forward'
+  },
+  {
+    key: '21', newImage: ImageWoman21 , name: 'Kaneryd', subName: 'Johanna',
+    number: '19', position: 'Winger'
+  },
+  {
+    key: '22', newImage: ImageWoman22 , name: 'James', subName: 'Lauren',
+    number: '10', position: 'Forward'
+  },
+  {
+    key: '23', newImage: ImageWoman23 , name: 'Kerr', subName: 'Sam',
+    number: '20', position: 'Forward'
+  },
+  
+]
